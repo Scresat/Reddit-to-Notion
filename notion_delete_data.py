247963f81a1f13942dd903708cd57fe3f67f4a4d
Scresat <32240792+Scresat@users.noreply.github.com>
2024-06-03 12:32:33 +0530

    Update notion_delete_data.py

--- notion_delete_data.py
+++ notion_delete_data.py
@@ -5,6 +5,7 @@ import time
 
 def delete_all_blocks(id_list):
     end_point = "https://api.notion.com/v1/blocks/"
+    
     for id in id_list:
         requests.delete(end_point + id, headers=config.headers)
         time.sleep(1)
